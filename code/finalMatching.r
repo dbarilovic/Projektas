38ae8efa8b94b1cd00398134e11bab94130dbab4
Ernesta Petraityte <ernesta.petraityte@yahoo.com>
2019-03-25 11:23:01 +0200

    pataisiau programa, kad sukurtu isfiltruotus failus pagal sablona

--- code/finalMatching.r
+++ code/finalMatching.r
@@ -1,21 +1,29 @@
+  GNU nano 2.7.4                                                                              File: finalMatching.r                                                                                         
+
 #!/usr/bin/env Rscript
 
-setwd("../output/")
+setwd("../output")
 patternLines <- readLines("pattern.txt")
-filelist = list.files(path="../output/columns/", pattern = ".*.columns") # failu pavadinimu listas
-filelistFull = list.files(path="../input/", pattern = "*.filtered") # failu pavadinimu listas
-setwd("../output/columns")
+filelist = list.files(path="columns", pattern = "*.columns$") # failu pavadinimu listas
+filelistFull = list.files(path="filtered", pattern = "*.filtered$") # failu pavadinimu listas
+#print (filelistFull)
+setwd("columns")
 count <- 0;
 for (name in filelist) {
 count <- count +1;
- fileCol <- readLines(name)
+fileCol <- readLines(name)
+#       fileCol <-readLines(filelist[1])
 fullFileNames <- filelistFull[count]
-setwd("../../input")
-fullFiles <- read.delim(fullFileNames)
-setwd("../output/final")
+setwd("../filtered")
+fullFiles <- read.table(fullFileNames)
+#       fullFiles <- read.delim(filelistFull[1])
+setwd("../final")
 final<-(paste(name,"final", sep="."))
 file.create(final)
-#print(fullFileNames)
-setwd("../output/columns")
+setwd("../columns")
 write.table(fullFiles[match(patternLines, fileCol),],final,sep=" ",row.names=FALSE, col.names=FALSE, quote=FALSE)
+        #print(fullFiles[match(patternLines, fileCol),])
+
 }
+
+
